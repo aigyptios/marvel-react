26ba961361a98f2d2dca2752cefcd26da7169c79
Mike Henry <m.henry511@gmail.com>
2019-11-11 13:49:46 +0200

    Add react and starter files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Hello, World! </h1>
+        <p>What's going on?</p>
+      </div>
+    );
+  }
+}
+
+export default App;
